da0450671ec89b2b12baeb10eb3ebd512f0b1cde
Belhanafi Abdelmadjid <112507960+BelhanafiAbdelmadjid@users.noreply.github.com>
2024-06-21 12:30:09 +0100

    fixinf bat

--- run_program.bat
+++ run_program.bat
@@ -1,12 +1,8 @@
-#!/bin/bash
-
 python -m venv venv 
-venv\Scripts\activate
+call venv\Scripts\activate
 
 pip install -r requirements.txt
-
+echo "Starting the app"
+start http://127.0.0.1:5000
 python web_app.py &
 
-sleep 5
-
-xdg-open http://127.0.0.1:5000
